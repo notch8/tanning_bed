2c802b7cb93387b9b469ce5af8482d7591fb17bc
Rob Kaufman <rob@notch8.com>
2008-12-15 18:56:06 -0800

    Getting there

--- lib/tanning_bed.rb
+++ lib/tanning_bed.rb
@@ -88,7 +88,7 @@ module TanningBed
 
 
   # :stopdoc:
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
