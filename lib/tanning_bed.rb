1a81a4d627e8bd8bc1c94486158b92d788571eba
Rob Kaufman <rob@notch8.com>
2008-12-18 11:26:29 -0800

    Some rake improvements for accessing the solr tasks from a gem

--- lib/tanning_bed.rb
+++ lib/tanning_bed.rb
@@ -116,7 +116,7 @@ module TanningBed
 
 
   # :stopdoc:
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
