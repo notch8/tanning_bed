9bc7d46e19fa6d6b18d0a33e6ef9bc9e9bcb02c6
Rob Kaufman <rob@notch8.com>
2008-12-22 18:40:21 -0800

    Improved rake tasks, adding solr_testing

--- tasks/solr.rb
+++ tasks/solr.rb
@@ -4,28 +4,18 @@ require 'net/http'
 require 'fileutils'
 
 namespace :solr do
+  
+  desc 'Start Solr for tests.  Sets SOLR_PORT to 8984 and index is puts in /tmp'
+  task :start_testing do
+    ENV["SOLR_PORT"] = '8984'
+    solr_command = "java -Dsolr.data.dir=/tmp -Djetty.port=#{solr_port} -jar start.jar"
+    start_solr(solr_command)
+  end
 
-  desc 'Starts Solr. PORT=XX.'
+  desc 'Starts Solr. SOLR_PORT=XX. SOLR_PATH=yy'
   task :start do
-    begin
-      n = Net::HTTP.new('localhost', solr_port)
-      n.request_head('/').value 
-
-    rescue Net::HTTPServerException #responding
-      puts "Port #{solr_port} in use" and return
-
-    rescue Errno::ECONNREFUSED #not responding
-      Dir.chdir(solr_path) do
-        pid = fork do
-          #STDERR.close
-          exec "java -Dsolr.data.dir=solr/data -Djetty.port=#{solr_port} -jar start.jar"
-        end
-        sleep(5)
-        FileUtils.mkdir_p("#{solr_path}/tmp")
-        File.open("#{solr_path}/tmp/solr_pid", "w"){ |f| f << pid}
-        puts "Solr started successfully on #{solr_port}, pid: #{pid}."
-      end
-    end
+    solr_command = "java -Dsolr.data.dir=solr/data -Djetty.port=#{solr_port} -jar start.jar"
+    start_solr(solr_command)
   end
   
   desc 'Stops Solr.'
@@ -69,6 +59,28 @@ namespace :solr do
       return ENV["SOLR_PATH"]
     else
       return "#{File.dirname(__FILE__)}/../vendor/solr"
+    end
+  end
+  
+  def start_solr(solr_command)
+    begin
+      n = Net::HTTP.new('localhost', solr_port)
+      n.request_head('/').value 
+
+    rescue Net::HTTPServerException #responding
+      puts "Port #{solr_port} in use" and return
+
+    rescue Errno::ECONNREFUSED #not responding
+      Dir.chdir(solr_path) do
+        pid = fork do
+          #STDERR.close
+          exec solr_command
+        end
+        sleep(5)
+        FileUtils.mkdir_p("#{solr_path}/tmp")
+        File.open("#{solr_path}/tmp/solr_pid", "w"){ |f| f << pid}
+        puts "Solr started successfully on #{solr_port}, pid: #{pid}."
+      end
     end
   end
 end
