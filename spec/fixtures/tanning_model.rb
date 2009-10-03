b20253cd348e61b4debf01b06f440142b8432b3d
Rob Kaufman <rob@notch8.com>
2009-10-02 19:23:23 -0700

    tanning bed array faceting example

--- spec/fixtures/tanning_model.rb
+++ spec/fixtures/tanning_model.rb
@@ -1,18 +1,23 @@
 class TanningModel
   include TanningBed
-  attr_accessor :name, :description, :tan_facet
+  attr_accessor :name, :description, :tan_facet, :keywords
   def initialize
     @name        = "Big Bad Voodoo Daddy"
     @description = "Not really that bad after all."
-    @tan_facet   =     "dark"
+    @tan_facet   = "dark"
+    @keywords = ["Record", "stuff", "other"]
   end
   
   def id
     666
   end
+
+  def keywords_facet
+    keywords
+  end
   
   
   def solr_keys
-    ["name", "description", "tan_facet"]
+    ["name", "description", "tan_facet", "keywords_facet"]
   end
 end
